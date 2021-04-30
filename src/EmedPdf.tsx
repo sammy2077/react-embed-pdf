3126b69c8b0ed8982968d9059faf0b98e59f3b4b
Samuel Dervis <sam@agweria.com>
2021-04-30 10:09:20 +0300

    Add eslint config

--- src/EmedPdf.tsx
+++ src/EmedPdf.tsx
@@ -1,19 +1,19 @@
-import React, { PureComponent } from 'react'
 import '@react-pdf-viewer/core/lib/styles/index.css';
+
 import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core';
+import React, { PureComponent } from 'react';
 
-
-type IProps={
-    url:string
-}
+type IProps = {
+    url: string;
+};
 export default class EmedPdf extends PureComponent<IProps> {
     render() {
         return (
             <div>
-                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.min.js">
-                                <Viewer fileUrl={this.props.url} defaultScale={SpecialZoomLevel.PageFit} plugins={[]} />
-                            </Worker>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.min.js">
+                    <Viewer fileUrl={this.props.url} defaultScale={SpecialZoomLevel.PageFit} plugins={[]} />
+                </Worker>
             </div>
-        )
+        );
     }
 }
