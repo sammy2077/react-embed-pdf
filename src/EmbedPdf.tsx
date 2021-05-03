902df125bdc4e1aafc481a23d9aa9e53d03ad9a7
Samuel Dervis <sam@agweria.com>
2021-05-04 00:52:10 +0300

    Add more deps

--- src/EmbedPdf.tsx
+++ src/EmbedPdf.tsx
@@ -1,7 +1,8 @@
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/print/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-import { SpecialZoomLevel, Viewer, ViewerProps } from '@react-pdf-viewer/core';
+import { SpecialZoomLevel, Viewer, ViewerProps, Worker } from '@react-pdf-viewer/core';
 import React, { PureComponent } from 'react';
 
 type IProps = {
@@ -12,7 +13,9 @@ export default class EmbedPdf extends PureComponent<IProps> {
     render() {
         return (
             <div className={this.props.classname}>
-                <Viewer fileUrl={this.props.fileUrl} defaultScale={SpecialZoomLevel.PageFit} plugins={[]} />
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.js">
+                    <Viewer fileUrl={this.props.fileUrl} defaultScale={SpecialZoomLevel.PageFit} plugins={[]} />
+                </Worker>
             </div>
         );
     }
